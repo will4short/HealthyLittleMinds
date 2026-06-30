44084236f3eebd274ccf61324aa8fc40108659d4
will4short <ucheambrose29@gmail.com>
2026-06-30 18:57:03 +0100

    commit all

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Lean precache (no big media) + runtime image cache
 // ===============================
 
-const PRECACHE_NAME = 'hlm-precache-v171'; // bump here for new release/update toast
+const PRECACHE_NAME = 'hlm-precache-v172'; // bump here for new release/update toast
 const RUNTIME_NAME  = 'hlm-runtime-v25';   // bump to refresh updated versioned page assets
 const IMAGE_CACHE   = 'hlm-img-v18';       // leave if image rules unchanged
 
